7d4f797b8c1979e77e3ab172e94a5885d7840b35
Fellipe <fellipe1311.fb@gmail.com>
2023-07-27 17:08:06 -0300

    database update

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -23,6 +23,20 @@ class DB {
 
   Future<void> _onCreate(Database db, int version) async {
     await db.execute(_fichas);
+    await db.execute(_tabelaTalentos);
+    await db.execute(_tabelaTalentoFicha);
+    await db.execute(_tabelaPericias);
+    await db.execute(_tabelaPericiasFicha);
+    await db.execute(_tabelaItens);
+    await db.execute(_tabelaItensFicha);
+    await db.execute(_tabelaHabilidades);
+    await db.execute(_tabelaHabilidadeFicha);
+    await db.execute(_tabelaMagias);
+    await db.execute(_tabelaMagiaFicha);
+    await db.execute(_tabelaClasses);
+    await db.execute(_tabelaClasseFicha);
+    await db.execute(_tabelaAtaques);
+    await db.execute(_tabelaAtaqueFicha);
   }
 
   String get _fichas => '''
@@ -33,82 +47,106 @@ class DB {
     tibar_prata INTEGER,
     tibar_ouro INTEGER,
     tibar_platina INTEGER,
-    xp_tual INTEGER
+    xp_tual INTEGER, 
+    ponto_acao INTEGER,
+    linguagens TEXT,
+    sexo TEXT,
+    tendencia TEXT,
+    tamanho TEXT,
+    resistencia TEXT,
+    deslocamento TEXT,
+    divindade TEXT,
+    bio TEXT,
+    imagem TEXT
   )
   ''';
 
-  Future<void> criarTabelaTalentos(int id) async {
-    final database = await instance.database;
+  String get _tabelaHabilidades => '''
+    CREATE TABLE habilidades(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    FOR INTEGER DEFAULT 10,
+    DES INTEGER DEFAULT 10,
+    CON INTEGER DEFAULT 10,
+    INT INTEGER DEFAULT 10,
+    SAB INTEGER DEFAULT 10,
+    CAR INTEGER DEFAULT 10,
+    mod_temp_FOR INTEGER,
+    mod_temp_DES INTEGER,
+    mod_temp_CON INTEGER,
+    mod_temp_INT INTEGER,
+    mod_temp_SAB INTEGER,
+    mod_temp_CAR INTEGER
+    mod_temp_FORT TEXT,
+    mod_temp_REF TEXT,
+    mod_temp_VON TEXT,
+    CA_outros INTEGER,
+    CA_temp INTEGER
+)''';
 
-    await database.execute('''
+  String get _tabelaHabilidadeFicha => '''
+    CREATE TABLE habilidade_ficha (
+    id_ficha INTEGER,
+    id_habilidade INTEGER,
+    FOREIGN KEY(id_ficha) REFERENCES fichas(id),
+    FOREIGN KEY(id_habilidade) REFERENCES habilidades(id)
+    )
+''';
+
+  String get _tabelaTalentos => '''
       CREATE TABLE talento (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome_talento TEXT,
-        descricao TEXT,
-        pre_requisito TEXT,
-        especial TEXT,
-        livro TEXT,
-        saved TEXT
-        )
-    ''');
-  }
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome_talento TEXT,
+      descricao TEXT,
+      pre_requisito TEXT,
+      especial TEXT,
+      livro TEXT,
+      saved TEXT
+      )
+    ''';
 
-  Future<void> criarTabelaTalento_ficha(int id) async {
-    final database = await instance.database;
-
-    await database.execute('''
+  String get _tabelaTalentoFicha => '''
       CREATE TABLE talento_ficha (
         id_ficha INTEGER,
         id_talento INTEGER,
         FOREIGN KEY(id_ficha) REFERENCES fichas(id),
         FOREIGN KEY(id_talento) REFERENCES talento(id)
         )
-    ''');
-  }
+    ''';
 
-  Future<void> criarTabelaPericias(int id) async {
-    final database = await instance.database;
-
-    await database.execute('''
+  String get _tabelaPericias => '''
       CREATE TABLE pericias (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome_pericia TEXT,
         somente_treinado TEXT,
         penalidade_armadura TEXT,
         )
-    ''');
+      
+      INSERT INTO pericias (nome_pericia, somente_treinado, penalidade_armadura)
+      VALUES
+      ( Adestrar Animais, s, n),
+      ( Acrobacia, n, s),
+      ( Atletismo, n, s),
+      ( Atuação, n, n),
+      ( Cavalgar, n, n),
+      ( Conhecimento, s, n),
+      ( Cura, n, n),
+      ( Diplomacia, n, n),
+      ( Enganação, n, n),
+      ( Furtividade, n, s),
+      ( Identificar Magia, s, n),
+      ( Iniciativa, n, n),
+      ( Intimidação, n, n),
+      ( Intuição, n, n),
+      ( Jogatina, n, n),
+      ( Ladinagem, s, s),
+      ( Meditação, n, n),
+      ( Obter Informação, n, n),
+      ( Ofício, n, n),
+      ( Percepção, n, n),
+      ( Sobrevivência, n, n)
+    ''';
 
-    await database.execute('''
-    INSERT INTO pericias (nome_pericia, somente_treinado, penalidade_armadura)
-    VALUES
-    ( Adestrar Animais, s, n),
-    ( Acrobacia, n, s),
-    ( Atletismo, n, s),
-    ( Atuação, n, n),
-    ( Cavalgar, n, n),
-    ( Conhecimento, s, n),
-    ( Cura, n, n),
-    ( Diplomacia, n, n),
-    ( Enganação, n, n),
-    ( Furtividade, n, s),
-    ( Identificar Magia, s, n),
-    ( Iniciativa, n, n),
-    ( Intimidação, n, n),
-    ( Intuição, n, n),
-    ( Jogatina, n, n),
-    ( Ladinagem, s, s),
-    ( Meditação, n, n),
-    ( Obter Informação, n, n),
-    ( Ofício, n, n),
-    ( Percepção, n, n),
-    ( Sobrevivência, n, n)
-''');
-  }
-
-  Future<void> criarTabelaPericias_ficha(int id) async {
-    final database = await instance.database;
-
-    await database.execute('''
+  String get _tabelaPericiasFicha => '''
       CREATE TABLE pericias_ficha (
         id_ficha INTEGER,
         id_pericia INTEGER,
@@ -119,49 +157,102 @@ class DB {
         FOREIGN KEY(id_ficha) REFERENCES fichas(id),
         FOREIGN KEY(id_pericia) REFERENCES pericias(id)
         )
-    ''');
-  }
+    ''';
 
-  Future<void> criarTabelaAtributos(int id) async {
-    final database = await instance.database;
-    final tableName = 'table_Atributos_$id';
-
-    await database.execute('''
-      CREATE TABLE $tableName (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT,
-        valor INTEGER,
-        temporario INTEGER 
-        )
-    ''');
-
-    await database.execute('''
-      INSERT INTO $tableName (nome, valor, temporario)
-      VALUES
-      ('FOR',10,0),
-      ('DES',10,0),
-      ('CON',10,0),
-      ('INT',10,0),
-      ('SAB',10,0),
-      ('CAR',10,0),
-
-''');
-  }
-
-  Future<void> criarTabelaItens(int id) async {
-    final database = await instance.database;
-    final tableName = 'table_Itens_$id';
-
-    await database.execute('''
-      CREATE TABLE $tableName (
+  String get _tabelaItens => '''
+      CREATE TABLE itens (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome_item TEXT,
         quantidade_item REAL,
         peso_item REAL,
         descricao_item TEXT
         )
-    ''');
-  }
+    ''';
+
+  String get _tabelaItensFicha => '''
+      CREATE TABLE itens_ficha (
+        id_ficha INTEGER,
+        id_item INTEGER,
+        FOREIGN KEY(id_ficha) REFERENCES fichas(id),
+        FOREIGN KEY(id_item) REFERENCES itens(id)
+        )
+    ''';
+
+  String get _tabelaMagias => '''
+    CREATE TABLE magias(
+      id PRIMARY KEY AUTOINCREMENT,
+      nome_magia TEXT,
+      alvo_area TEXT,
+      tempo_execucao TEXT,
+      escola TEXT,
+      duracao TEXT,
+      alcance TEXT,
+      teste_resistencia TEXT,
+      memorizada INTEGER,
+      descricao TEXT
+    )
+''';
+
+  String get _tabelaMagiaFicha => '''
+    CREATE TABLE magia_fica(
+        id_ficha INTEGER,
+        id_magia INTEGER,
+        FOREIGN KEY(id_ficha) REFERENCES fichas(id),
+        FOREIGN KEY(id_magia) REFERENCES magias(id)
+    )
+''';
+
+  String get _tabelaClasses => '''
+    CREATE TABLE classes(
+      id INTEGER PRIMARY KEY,
+      classe_1 TEXT,
+      classe_2 TEXT,
+      classe_3 TEXT,
+      classe_4 TEXT,
+      pv_classe_1 INTEGER,
+      pv_classe_2 INTEGER,
+      pv_classe_3 INTEGER,
+      pv_classe_4 INTEGER,
+      bba_classe_1 INTEGER,
+      bba_classe_2 INTEGER,
+      bba_classe_3 INTEGER,
+      bba_classe_4 INTEGER,
+      pv_temp INTEGER,
+      mana_total INTEGER,
+      mana_atual INTEGER,
+      energia INTEGER
+    )
+''';
+
+  String get _tabelaClasseFicha => '''
+    CREATE TABLE magia_fica(
+      id_ficha INTEGER,
+      id_classe INTEGER,
+      FOREIGN KEY(id_ficha) REFERENCES fichas(id),
+      FOREIGN KEY(id_classe) REFERENCES classes(id)
+    )
+''';
+
+  String get _tabelaAtaques => '''
+    CREATE TABLE ataques(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome_ataque TEXT,
+      bonus_ataque INTEGER,
+      dano TEXT,
+      critico TEXT,
+      alcance TEXT,
+      tipo TEXT
+    )
+''';
+
+  String get _tabelaAtaqueFicha => '''
+    CREATE TABLE ataque_ficha(
+      id_ficha INTEGER,
+      id_ataque INTEGER,
+      FOREIGN KEY(id_ficha) REFERENCES fichas(id),
+      FOREIGN KEY(id_ataques) REFERENCES ataques(id)
+    )
+''';
 
 //------------------------------------------------------------------------------
 
@@ -181,23 +272,16 @@ class DB {
 
   Future<void> inserirNome(String nome) async {
     final database = await instance.database;
-    late final int? id;
     await database.transaction((txn) async {
       await txn.insert(
         'fichas',
         {'nome_personagem': nome},
       );
     });
-    id = await buscarIdPorNome(nome);
-    await criarTabelaTalentos(id);
-    await criarTabelaPericias(id);
-    await criarTabelaAtributos(id);
-    await criarTabelaItens(id);
   }
 
   Future<void> inserirCaracteristica(String nome, String caracteristica) async {
     final database = await instance.database;
-
     await database.insert(
       nome,
       {'caracteristica': caracteristica},
